b9f0b8a85f9ce6445d68358815da5b6a6ac7fbdc
cruz <edwin.cruz.v@outlook.com>
2022-03-04 22:41:05 -0600

    Graficas y RRD completos

--- Practica1/main.py
+++ Practica1/main.py
@@ -1,71 +1,160 @@
-from Libreria.GetSNMP import *
-from Libreria.CreateRRD import *
+from GetSNMP import *
+import rrdtool
+import time
+import json
 
-
-def mostrarConsulta(comunidad:str,host:str,str_mib:str)->None:
+# se encarga de hacer el get y regresar lo que queremos 
+def mostrarConsulta(comunidad:str,host:str,str_mib:str)->str:
     traffic=""
-    traffic=consultaSNMP('123','localhost','1.3.6.1.2.1.2.2.1.10.2')
+    traffic=consultaSNMP(comunidad,host,str_mib)
     print (traffic)
+    return traffic
 
-def nuevaRDD():
-    pass
+# Crea una base de datos en base al nombre y para un solo dato
+def nuevaRDD(nombreRRD:str):
+    # creamos la bae de datos
+    ret = rrdtool.create(nombreRRD,
+                        "--start", # momento que se empieza a almacenar los datos
+                        'N',# now (empieza al momento de ejecutar el script) (tambien un numero)
+                        "--step", # un step
+                        '30',      # cada minuto
+    #DS: Octetos de entrada : Tipo contador : cada dos minutos : sin limites minumos : sin limites maximos
+                        "DS:octets:COUNTER:30:U:U",
+    #RRA: Cada 60 segs de hace un AVERAGE: la mitad de muestras se validan: cada 1 step : numero de filas en la base de datos
+                        "RRA:AVERAGE:0.5:1:32") #32 filas,cada uno de 30 segs
+
+    #en caso de haber un error lo sabremos
+    if ret:
+        print (rrdtool.error())
 
 
+# se encarga de crear las bases que necesitamos
+def creaBases():
+    nuevaRDD("multicast.rrd")
+    nuevaRDD("ipv4.rrd")
+    nuevaRDD("icmp.rrd")
+    nuevaRDD("octets.rrd")
+    nuevaRDD("ports.rrd")
 
-def listaConsultas():
-    comunidad="123"
-    host="localhost"
+# realiza las consultas que necesitamos
+def listaConsultas(comunidad:str,host:str):
+    
     str_mib="1.3.6.1.2.1."
     print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
-    # 1) Paquetes unicast que ha recibido una interfaz (ifInUcastPkts) 1.3.6.1.2.1.2.2.1.11 counter
-    print("Paquetes unicast que ha recibido una interfaz (")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.11")
-    # 2) Paquetes multicast que ha recibido una interfaz (ifInNUcastPkts) 1.3.6.1.2.1.2.2.1.12 counter
-    print("Paquetes multicast que ha recibido una interfaz")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.12")
-    # 3) Paquetes multicast que ha enviado una interfaz (ifOutNUcastPkts) 1.3.6.1.2.1.2.2.1.18 counter
-    print("Paquetes multicast que ha enviado una interfaz")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.18")
 
-    # 1) Paquetes recibidos a protocolos IPv4, incluyendo los que tienen errores. (ipInReceives) 1.3.6.1.2.1.4.3.0 counter
-    print("Paquetes recibidos a protocolos IPv4, incluyendo los que tienen errores")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.4.3.0")
-    # 2) Paquetes recibidos exitosamente, entregados a protocolos IPv4. (ipInDelivers) 1.3.6.1.2.1.4.9.0 counter
-    print("Paquetes recibidos exitosamente, entregados a protocolos IPv4")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.4.9.0")
-    # 3) Paquetes Ipv4 que los protocolos locales de usuarios de IPv4 suministraron a IPv4 en las solicitudes de transmisión. (ipOutRequests) 1.3.6.1.2.1.4.10.0 counter
-    print("Paquetes Ipv4 que los protocolos locales de usuarios de IPv4 suministraron a IPv4 en las solicitudes de transmisión.")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.4.10.0")
+    # print("2) Paquetes multicast que ha recibido una interfaz (ifInNUcastPkts) 1.3.6.1.2.1.2.2.1.12.1 counter")
+    valor = "N:" + mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.12.1")
+    rrdtool.update('multicast.rrd', valor)
+    rrdtool.dump('multicast.rrd','multicast.xml')
+    # print("2) Paquetes recibidos exitosamente, entregados a protocolos IPv4. (ipInDelivers) 1.3.6.1.2.1.4.9.0 counter")
+    valor = "N:" + mostrarConsulta(comunidad, host, "1.3.6.1.2.1.4.9.0")
+    rrdtool.update('ipv4.rrd', valor)
+    rrdtool.dump('ipv4.rrd','ipv4.xml')
+    # print("2) Mensajes de respuesta ICMP que ha enviado el agente (icmpOutEchoReps) 1.3.6.1.2.1.5.22.0 counter")
+    valor = "N:" + mostrarConsulta(comunidad, host, "1.3.6.1.2.1.5.22.0")
+    rrdtool.update('icmp.rrd', valor)
+    rrdtool.dump('icmp.rrd','icmp.xml')
+    # print("2) Segmentos enviados, incluyendo los de las conexiones actuales, ",
+    #     "pero excluyendo los que contienen solamente octetos retransmitidos (ifOutOctets) 1.3.6.1.2.1.2.2.1.16.1 counter")
+    valor = "N:" + mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.16.1")
+    rrdtool.update('octets.rrd', valor)
+    rrdtool.dump('octets.rrd','octets.xml')
+    # print("D2) Datagramas recibidos que no pudieron ser entregados por cuestiones ",
+    #     "distintas a la falta de aplicación en el puerto destino (udpNoPorts) 1.3.6.1.2.1.7.2.0 counter")
+    valor = "N:" + mostrarConsulta(comunidad, host, "1.3.6.1.2.1.7.2.0")
+    rrdtool.update('ports.rrd', valor)
+    rrdtool.dump('ports.rrd','ports.xml')
 
-    # 1) Mensajes ICMP echo que ha enviado el agente (icmpOutEchos) 1.3.6.1.2.1.5.21.0 counter
-    print("Mensajes ICMP echo que ha enviado el agente ")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.5.21.0")
-    # 2) Mensajes de respuesta ICMP que ha enviado el agente (icmpOutEchoReps) 1.3.6.1.2.1.5.22.0 counter
-    print("Mensajes de respuesta ICMP que ha enviado el agente")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.5.22.0")
-    # 3) Mensajes ICMP que ha recibido el agente. (icmpInEchos) 1.3.6.1.2.1.5.8.0 counter
-    print("Mensajes ICMP que ha recibido el agente")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.5.8.0")
 
-    # 1) Segmentos recibidos, incluyendo los que se han recibido con errores. (ifInOctets) 1.3.6.1.2.1.2.2.1.10 counter
-    print("Segmentos recibidos, incluyendo los que se han recibido con errores")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.10")
-    # 2) Segmentos enviados, incluyendo los de las conexiones actuales, pero excluyendo los que contienen solamente octetos retransmitidos (ifOutOctets) 1.3.6.1.2.1.2.2.1.16 counter
-    print("Segmentos enviados, incluyendo los de las conexiones actuales, pero excluyendo los que contienen solamente octetos retransmitidos")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.2.2.1.16")
-    # 3) Segmentos retransmitidos; es decir, el número de segmentos TCP transmitidos que contienen uno o más octetos transmitidos previamente 
-    print("Segmentos retransmitidos; es decir, el número de segmentos TCP transmitidos que contienen uno o más octetos transmitidos previamente")
-    #mostrarConsulta(comunidad, host, str_mib)
+def nuevaGrafica(nombre_grafica:str,base_RRD:str):
+    # tiempo actual
+    tiempo_actual = int(time.time())
+    #Grafica desde el tiempo actual menos diez minutos
+    tiempo_inicial = tiempo_actual - 690 #16 minutos lo que dura el analisis
+    # solo se generara una grafica en base al tradico de red
+    ret = rrdtool.graph( nombre_grafica,
+                        "--start",str(tiempo_inicial),
+                        "--end","N",
+                        "--vertical-label=Bytes/s", # maquillaje para la grafica
+                        "--title=Tráfico de Red de un agente \n Usando SNMP y RRDtools", # maquillaje para la grafica
+                        # se consulta la informacion para graficarlo
+                        "DEF:traficoEntrada="+base_RRD+":octets:AVERAGE",
+                        # recorre toda la coleccion para convertir los octetos a bites
+                        "CDEF:escalaIn=traficoEntrada,8,*",
+                        # Para imprimir los datos
+                        "LINE3:escalaIn#00FF00:Trafico de entrada")
 
-    # 1) Datagramas entregados a usuarios UDP (udpInDatagrams) 1.3.6.1.2.1.7.1.0 counter
-    print("Datagramas entregados a usuarios UDP")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.7.1.0")
-    # 2) Datagramas recibidos que no pudieron ser entregados por cuestiones distintas a la falta de aplicación en el puerto destino (udpNoPorts) 1.3.6.1.2.1.7.2.0 counter
-    print("Datagramas recibidos que no pudieron ser entregados por cuestiones distintas a la falta de aplicación en el puerto destino")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.7.2.0")
-    # 3) Datagramas enviados por el dispositivo. (udpOutDatagrams) 1.3.6.1.2.1.7.4.0 counter
-    print("Datagramas enviados por el dispositivo")
-    mostrarConsulta(comunidad, host, "1.3.6.1.2.1.7.4.0")
- 
-while True:
-    listaConsultas()
+def creaGraficas():
+    nuevaGrafica("multicast.png",'multicast.rrd')
+    nuevaGrafica("ipv4.png",'ipv4.rrd')
+    nuevaGrafica("icmp.png",'icmp.rrd')
+    nuevaGrafica("octets.png",'octets.rrd')
+    nuevaGrafica("ports.png",'ports.rrd')
+
+
+def trafico(comunidad:str, host:str):
+    listaConsultas(comunidad, host)
+    creaBases()
+    inicio=time.time()
+    fin= inicio + 960# 16 minutos
+    while True:
+        listaConsultas()
+        inicio=time.time()
+        if not inicio < fin:
+            break
+    creaGraficas()
+
+class Agentes:
+    
+
+    def __init__(self):
+        self.temp_list=[]
+        with open("agentes.json",'r') as temp_file:
+            self.temp_list=json.load(temp_file)
+        print("Agentes actuales",self.temp_list)
+
+    def agregar(self,host:str):
+        self.temp_list.append(host)
+
+    def eliminar(self,host:str):
+        pass
+
+    def actualizar(self):
+        with open("agentes.json",'w') as temp_file:
+            json.dump(self.temp_list,temp_file)
+            
+
+if __name__=='__main__':
+
+    comunidad="123"
+    agentes=Agentes()
+    
+
+    opcion=0
+
+    while True:
+        print("Menu:\n1)Alta.\n2.)Baja.\n3)ver trafico y generar reporte.\n4)Salir\n")
+        opcion=int(input("Escriba la opcion: "))
+        if opcion==1:
+            nuevo_host=input("Escriba un host valido: ")
+            # agentes.agregar("192.168.1.66")
+            agentes.actualizar()
+            print("Agregado")
+            sleep(3)
+        elif opcion==2:
+            fuera_host=input("Escriba el host a eliminar: ")
+            # agente.eliminar()
+            agentes.actualizar()
+            print("Eliminado")
+            sleep(3)
+        elif opcion==3:
+            reporte_host=input("escriba el host a examinar: ")
+            print("Espere 16 min en lo que se genera el reporte... en un momento comienza..")
+            sleep(5)
+        elif opcion==4:
+            break
+        else:
+            continue
+    
+
+    
